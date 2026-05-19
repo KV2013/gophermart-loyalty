63db2a8829c73ba2c34e6a2bc2941d6b43ce0eca
Kilikaev Victor <kv2013@gmail.com>
2026-05-19 23:49:18 +0500

    улутшение: добавил логирование в middlware.Api

--- internal/middleware/api.go
+++ internal/middleware/api.go
@@ -3,20 +3,27 @@ package middleware
 import (
 	"net/http"
 	"strings"
+
+	"go.uber.org/zap"
 )
 
-func Api(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasPrefix(r.URL.Path, "/api") {
-			if !(r.Method == http.MethodPost && r.URL.Path == "/api/user/orders") {
-				if !strings.HasPrefix(r.Header.Get("Content-Type"), "application/json") {
-					w.Header().Set("Content-Type", "application/json")
-					w.WriteHeader(http.StatusBadRequest)
-					return
+func Api(l *zap.Logger) func(next http.Handler) http.Handler {
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, "/api") {
+				if !(r.Method == http.MethodPost && r.URL.Path == "/api/user/orders") {
+					if !strings.HasPrefix(r.Header.Get("Content-Type"), "application/json") {
+						l.Debug("не валидный заголовок Content-Type", zap.String("request_header", r.Header.Get("Content-Type")))
+						w.Header().Set("Content-Type", "application/json")
+						w.WriteHeader(http.StatusBadRequest)
+						return
+					}
 				}
+				w.Header().Set("Content-Type", "application/json")
 			}
-			w.Header().Set("Content-Type", "application/json")
-		}
-		next.ServeHTTP(w, r)
-	})
+			next.ServeHTTP(w, r)
+		})
+	}
+
 }
